5316049ae50e279ff5a97207aa3bef7b6b344690
Reuven Karasik <reuven.karasik@connecteam.com>
2026-04-25 22:32:19 +0300

    fix: restore setupFilesAfterEnv in jest config (jest-expo@55 incompatible with Expo SDK 54)

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   testEnvironment: 'node',
   setupFiles: ['<rootDir>/jest.setup.js'],
+  setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/$1',
@@ -9,7 +10,7 @@ module.exports = {
     '^.+\\.(ts|tsx|js|jsx)$': 'babel-jest',
   },
   transformIgnorePatterns: [
-    'node_modules/(?!((jest-)?react-native|@react-native(-community)?|@react-navigation|@supabase|@clerk|@testing-library|expo(nent)?|@expo(nent)?/.*)/)',
+    'node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg|@clerk/.*|@supabase/.*)',
   ],
   testPathIgnorePatterns: ['/node_modules/', '/__tests__/test-utils\\.tsx'],
 }
